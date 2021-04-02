a999363b51061023efff57ad95e9343f2ba0de0c
Chidi Williams <williamschidi1@gmail.com>
2021-04-02 20:12:43 +0100

    Update bookshelf

--- shelf.sh
+++ shelf.sh
@@ -1,8 +1,8 @@
 # Updates the /shelf page. Uses the script at https://github.com/chidiwilliams/scripts/tree/master/goodreads-website-shelf
 # to pull in my bookshelf from Goodreads.
 
-curl -L "https://github.com/chidiwilliams/scripts/archive/v1.tar.gz" | tar -xz
-cd scripts-1/goodreads-website-shelf
+curl -L "https://github.com/chidiwilliams/scripts/archive/v1.1.tar.gz" | tar -xz
+cd scripts-1.1/goodreads-website-shelf
 npm install
 node index.js >../../data/shelf.json
-cd ../../ && rm -rf scripts-1/
+cd ../../ && rm -rf scripts-1.1/
